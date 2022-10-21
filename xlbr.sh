c447e8815033b0e43cc7bd2aa73b65eb84a23609
sadhukhanr <rohit02sadhukhan@gmail.com>
2022-10-21 16:51:43 +0530

    update xlbr.sh

--- xlbr.sh
+++ xlbr.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+
 red='\e[1;31m'
 black='\e[1;30m'
 green='\e[1;32m'
@@ -6,138 +7,429 @@ cayan='\e[1;36m'
 yellow='\e[1;33m'
 white='\e[1;37m'
 reset='\e[1;0m'
-while getopts b:c:n: flag;
-do
-    case "${flag}" in
-        b) base=${OPTARG}
-        echo -e $green
-        echo -e "xlbr.sh \nlog \nnode_modules \nremote" > .gitignore
-        mkdir src
-        echo "creating filesystem..."
-        cd ./src/
-        mkdir frm
-        mkdir fnt
-        mkdir img
-        mkdir pug
-        mkdir css
-        echo -e "created frm..."
-        echo -e "created fnt..."
-        echo -e "created img..."
-        echo -e "created pug..."
-        cd css 
-        echo "/*index.css ~ sadhukhanr*/" > index.css
-        echo "//index.scss ~ sadhukhanr" > index.scss
-        echo -e "generated index.css... "
-        echo -e "generated index.scss... "
-        cd ..
-        mkdir js
-        cd js
-        echo -e "created js..."
-        echo "//logic.js ~ sadhukhanr" > logic.js
-        echo -e "generated logic.js... "
-        cd .. 
-        echo -e "created ng-route..."
-        mkdir ng-route
-        echo -e "created json..."
-        echo -e "generated data.json... "
-        mkdir json
-        cd json
-        touch data.json
-        cd ..
-        cd ..
-        echo -e "created doc..."
-        mkdir doc
-        cd doc
-        echo -e "# Tools \nthis project is created with excalibur \n[github](https://github.com/sadhukhanr/excalibur)" > tool.md
-        cd ..
-        echo -e "adding ISSUE_TEMPLATE"
-        echo -e $reset
-        mkdir .github
-        cd .github
-        mkdir ISSUE_TEMPLATE
-        cd ISSUE_TEMPLATE
-        curl https://raw.githubusercontent.com/sadhukhanr/open-source/main/.github/ISSUE_TEMPLATE/bug_report.md > bug_report.md
-        curl https://raw.githubusercontent.com/sadhukhanr/open-source/main/.github/ISSUE_TEMPLATE/feature_request.md > feature_request.md
-        echo -e $red
-        echo -e "generated bug_report.md... "
-        echo -e "generated feature_request.md... "
-        cd ..
-        cd ..
-        echo -e $reset
-        echo -e $green
-        echo -e "adding code_of_conduct.md & contributing.md..."
-        echo -e $reset
-        curl https://raw.githubusercontent.com/sadhukhanr/open-source/main/code_of_conduct.md > code_of_conduct.md
-        curl https://raw.githubusercontent.com/sadhukhanr/open-source/main/contributing.md > contributing.md
-        echo -e $green
-        echo -e "generated code_of_conduct.md... "
-        echo -e "generated contributing.md... "
-        touch index.html
-        touch index.pug
-        touch LICENSE
-        echo -e "generated index.html... "
-        echo -e "generated index.pug... "
-        echo -e $reset
-        echo -e $yellow
-        echo "git initialization...."
-        echo -e $reset
-        echo -e $reset
-        git init
-        git branch -m master main
-        echo -e $green
-        read -p "github user_name:" x
-        read -p "github remote repo_name:" y
-        git remote add origin "git@github.com:$x/$y.git"
-        echo -e $reset
-        git remote -v > remote
-        echo -e "# $y \n$x" > README.md
-        echo -e "generated remote... "
-        echo -e "generated README.md... "
-        echo -e $yellow
-        echo "node initialization...."
-        echo -e $reset
-        npm init
-        git add .
-        git commit -m "commit by xlbr: created base system..."
-        git log > log
-        echo "finished!...";;
-        c) msgg=${OPTARG}
-           echo "making commit..."
-           read -p "enter message:" msg
-           read -p "branch:" bitch
-           git add .
-           git commit -m $msg
-           rm -rf log
-           git log > log
-           git push --set-upstream origin $bitch ;;
-        n) frem=${OPTARG}
-        cd ./src/frm/
-        #bootstrap
-        if [[ "$frem" == "bootstrap" ]]; then
-        echo "installing bootstrap...."
-             mkdir bootstrap
-             cd bootstrap
-             curl https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css > bootstrap.min.css
-             curl https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js > bootstrap.min.js
-             cd .. 
-        fi
-        #jquery
-        if [[ "$frem" == "jquery" ]]; then
-        echo "installing jquery...."
-             mkdir jquery
-             cd jquery
-             curl https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js > jquery.min.js
-             cd .. 
-        fi
-        #tailwind
-        if [[ "$frem" == "tailwind" ]]; then
-        echo "installing tailwind...."
-             mkdir tailwind
-             cd tailwind
-             curl https://cdn.tailwindcss.com/3.1.8 > tailwind.js
-             cd .. 
-        fi
 
-        cd .. ;;
-    esac
+function Pbar {
+  let _progress=(${1} * 100 / ${2} * 100)/100
+  let _done=(${_progress} * 4)/10
+  let _left=40-$_done
+  _fill=$(printf "%${_done}s")
+  _empty=$(printf "%${_left}s")
+  printf "\rProgress : $green [${_fill// /#}${_empty// /-}] ${_progress}%% $reset"
+}
+
+_start=1
+_end=100
+
+for number in $(seq ${_start} ${_end}); do
+  Pbar ${number} ${_end}
+done
+printf '\nFinished!\n'
+
+mkdir css
+mkdir js
+mkdir img
+mkdir fnt
+mkdir scss
+mkdir cmn
+
+touch index.html
+touch index.pug
+
+cd scss
+touch _main.scss
+cd ..
+
+cd css
+touch style.css
+cd ..
+
+cd js
+touch logic.js
+cd ..
+
+git init
+git branch -m master main
+read -p "github user_name:" x
+read -p "github remote repo_name:" y
+git remote add origin "git@github.com:$x/$y.git"
+echo -e "# $y \n$x" >README.md
+echo -e "git remote:"
+echo -e $green
+git remote -v
+echo -e $reset
+sleep 5
+echo -e "xlbr.sh" >.gitignore
+
+npm init
+git add .
+git commit -m "commited by xlbr: generated base"
+git log
+sleep 5
+
+year=$(date +%Y)
+
+read -p "your github mail address:" z
+echo -e "
+# Contributor Covenant Code of Conduct
+
+## Our Pledge
+
+We as members, contributors, and leaders pledge to make participation in our
+community a harassment-free experience for everyone, regardless of age, body
+size, visible or invisible disability, ethnicity, sex characteristics, gender
+identity and expression, level of experience, education, socio-economic status,
+nationality, personal appearance, race, religion, or sexual identity
+and orientation.
+
+We pledge to act and interact in ways that contribute to an open, welcoming,
+diverse, inclusive, and healthy community.
+
+## Our Standards
+
+Examples of behavior that contributes to a positive environment for our
+community include:
+
+* Demonstrating empathy and kindness toward other people
+* Being respectful of differing opinions, viewpoints, and experiences
+* Giving and gracefully accepting constructive feedback
+* Accepting responsibility and apologizing to those affected by our mistakes,
+  and learning from the experience
+* Focusing on what is best not just for us as individuals, but for the
+  overall community
+
+Examples of unacceptable behavior include:
+
+* The use of sexualized language or imagery, and sexual attention or
+  advances of any kind
+* Trolling, insulting or derogatory comments, and personal or political attacks
+* Public or private harassment
+* Publishing others' private information, such as a physical or email
+  address, without their explicit permission
+* Other conduct which could reasonably be considered inappropriate in a
+  professional setting
+
+## Enforcement Responsibilities
+
+Community leaders are responsible for clarifying and enforcing our standards of
+acceptable behavior and will take appropriate and fair corrective action in
+response to any behavior that they deem inappropriate, threatening, offensive,
+or harmful.
+
+Community leaders have the right and responsibility to remove, edit, or reject
+comments, commits, code, wiki edits, issues, and other contributions that are
+not aligned to this Code of Conduct, and will communicate reasons for moderation
+decisions when appropriate.
+
+## Scope
+
+This Code of Conduct applies within all community spaces, and also applies when
+an individual is officially representing the community in public spaces.
+Examples of representing our community include using an official e-mail address,
+posting via an official social media account, or acting as an appointed
+representative at an online or offline event.
+
+## Enforcement
+
+Instances of abusive, harassing, or otherwise unacceptable behavior may be
+reported to the community leaders responsible for enforcement at $z.
+All complaints will be reviewed and investigated promptly and fairly.
+
+All community leaders are obligated to respect the privacy and security of the
+reporter of any incident.
+
+## Enforcement Guidelines
+
+Community leaders will follow these Community Impact Guidelines in determining
+the consequences for any action they deem in violation of this Code of Conduct:
+
+### 1. Correction
+
+**Community Impact**: Use of inappropriate language or other behavior deemed
+unprofessional or unwelcome in the community.
+
+**Consequence**: A private, written warning from community leaders, providing
+clarity around the nature of the violation and an explanation of why the
+behavior was inappropriate. A public apology may be requested.
+
+### 2. Warning
+
+**Community Impact**: A violation through a single incident or series
+of actions.
+
+**Consequence**: A warning with consequences for continued behavior. No
+interaction with the people involved, including unsolicited interaction with
+those enforcing the Code of Conduct, for a specified period of time. This
+includes avoiding interactions in community spaces as well as external channels
+like social media. Violating these terms may lead to a temporary or
+permanent ban.
+
+### 3. Temporary Ban
+
+**Community Impact**: A serious violation of community standards, including
+sustained inappropriate behavior.
+
+**Consequence**: A temporary ban from any sort of interaction or public
+communication with the community for a specified period of time. No public or
+private interaction with the people involved, including unsolicited interaction
+with those enforcing the Code of Conduct, is allowed during this period.
+Violating these terms may lead to a permanent ban.
+
+### 4. Permanent Ban
+
+**Community Impact**: Demonstrating a pattern of violation of community
+standards, including sustained inappropriate behavior,  harassment of an
+individual, or aggression toward or disparagement of classes of individuals.
+
+**Consequence**: A permanent ban from any sort of public interaction within
+the community.
+
+## Attribution
+
+This Code of Conduct is adapted from the [Contributor Covenant][homepage],
+version 2.0, available at
+https://www.contributor-covenant.org/version/2/0/code_of_conduct.html.
+
+Community Impact Guidelines were inspired by [Mozilla's code of conduct
+enforcement ladder](https://github.com/mozilla/diversity).
+
+[homepage]: https://www.contributor-covenant.org
+
+For answers to common questions about this code of conduct, see the FAQ at
+https://www.contributor-covenant.org/faq. Translations are available at
+https://www.contributor-covenant.org/translations.
+" >code_of_conduct.md
+echo -e "
+# Contributing
+
+When contributing to this repository, please first discuss the change you wish to make via issue,
+email, or any other method with the owners of this repository before making a change. 
+
+Please note we have a code of conduct, please follow it in all your interactions with the project.
+
+## Pull Request Process
+
+1. Ensure any install or build dependencies are removed before the end of the layer when doing a 
+   build.
+2. Update the README.md with details of changes to the interface, this includes new environment 
+   variables, exposed ports, useful file locations and container parameters.
+3. Increase the version numbers in any examples files and the README.md to the new version that this
+   Pull Request would represent. The versioning scheme we use is [SemVer](http://semver.org/).
+4. You may merge the Pull Request in once you have the sign-off of two other developers, or if you 
+   do not have permission to do that, you may request the second reviewer to merge it for you.
+
+## Code of Conduct
+
+### Our Pledge
+
+In the interest of fostering an open and welcoming environment, we as
+contributors and maintainers pledge to making participation in our project and
+our community a harassment-free experience for everyone, regardless of age, body
+size, disability, ethnicity, gender identity and expression, level of experience,
+nationality, personal appearance, race, religion, or sexual identity and
+orientation.
+
+### Our Standards
+
+Examples of behavior that contributes to creating a positive environment
+include:
+
+* Using welcoming and inclusive language
+* Being respectful of differing viewpoints and experiences
+* Gracefully accepting constructive criticism
+* Focusing on what is best for the community
+* Showing empathy towards other community members
+
+Examples of unacceptable behavior by participants include:
+
+* The use of sexualized language or imagery and unwelcome sexual attention or
+advances
+* Trolling, insulting/derogatory comments, and personal or political attacks
+* Public or private harassment
+* Publishing others' private information, such as a physical or electronic
+  address, without explicit permission
+* Other conduct which could reasonably be considered inappropriate in a
+  professional setting
+
+### Our Responsibilities
+
+Project maintainers are responsible for clarifying the standards of acceptable
+behavior and are expected to take appropriate and fair corrective action in
+response to any instances of unacceptable behavior.
+
+Project maintainers have the right and responsibility to remove, edit, or
+reject comments, commits, code, wiki edits, issues, and other contributions
+that are not aligned to this Code of Conduct, or to ban temporarily or
+permanently any contributor for other behaviors that they deem inappropriate,
+threatening, offensive, or harmful.
+
+### Scope
+
+This Code of Conduct applies both within project spaces and in public spaces
+when an individual is representing the project or its community. Examples of
+representing a project or community include using an official project e-mail
+address, posting via an official social media account, or acting as an appointed
+representative at an online or offline event. Representation of a project may be
+further defined and clarified by project maintainers.
+
+### Enforcement
+
+Instances of abusive, harassing, or otherwise unacceptable behavior may be
+reported by contacting the project team at [$z](). All
+complaints will be reviewed and investigated and will result in a response that
+is deemed necessary and appropriate to the circumstances. The project team is
+obligated to maintain confidentiality with regard to the reporter of an incident.
+Further details of specific enforcement policies may be posted separately.
+
+Project maintainers who do not follow or enforce the Code of Conduct in good
+faith may face temporary or permanent repercussions as determined by other
+members of the project's leadership.
+
+### Attribution
+
+This Code of Conduct is adapted from the [Contributor Covenant][homepage], version 1.4,
+available at [http://contributor-covenant.org/version/1/4][version]
+
+[homepage]: http://contributor-covenant.org
+[version]: http://contributor-covenant.org/version/1/4/
+" >contributing.md
+echo -e "
+MIT License
+
+Copyright (c) $year $x
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the ""Software""), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+" >LICENSE
+mkdir .github
+cd .github
+mkdir ISSUE_TEMPLATE
+cd ISSUE_TEMPLATE
+echo -e "
+---
+name: Bug report
+about: Create a report to help us improve
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+**Describe the bug**
+A clear and concise description of what the bug is.
+
+**To Reproduce**
+Steps to reproduce the behavior:
+1. Go to '...'
+2. Click on '....'
+3. Scroll down to '....'
+4. See error
+
+**Expected behavior**
+A clear and concise description of what you expected to happen.
+
+**Screenshots**
+If applicable, add screenshots to help explain your problem.
+
+**Desktop (please complete the following information):**
+ - OS: [e.g. iOS]
+ - Browser [e.g. chrome, safari]
+ - Version [e.g. 22]
+
+**Smartphone (please complete the following information):**
+ - Device: [e.g. iPhone6]
+ - OS: [e.g. iOS8.1]
+ - Browser [e.g. stock browser, safari]
+ - Version [e.g. 22]
+
+**Additional context**
+Add any other context about the problem here.
+" >bug_report.md
+echo -e "
+---
+name: Feature request
+about: Suggest an idea for this project
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+**Is your feature request related to a problem? Please describe.**
+A clear and concise description of what the problem is. Ex. I'm always frustrated when [...]
+
+**Describe the solution you'd like**
+A clear and concise description of what you want to happen.
+
+**Describe alternatives you've considered**
+A clear and concise description of any alternative solutions or features you've considered.
+
+**Additional context**
+Add any other context or screenshots about the feature request here.
+" >feature_request.md
+cd ..
+cd ..
+echo -e "
+root/
+ .gitignore
+ index.css
+ css/
+  style.css
+  bootstrap.min.css
+ js/
+  logic.js
+  tf.min.js
+ img/
+  one.png
+  xlbr.svg
+ fnt
+  new.ttf
+  black_g.ttf
+ scss/
+  _main.scss
+  _btn.scss 
+ cmn/
+  page2.html
+  page3.html
+  page2.pug
+  page3.pug
+
+" >file_structure.txt
+rm -rf .gitignore
+echo -e "xlbr.sh \nfile_structure.txt" >.gitignore
+while true; do
+  read -p "Do you wanna make this project open source?" yn
+  case $yn in
+  [Yy]*)
+    git add .
+    git commit -m "commited by xlbr: create LICENSE and other stuff"
+    git log
+    rm -- "$0"
+    exit
+    ;;
+
+  [Nn]*)
+    rm -rf .github
+    rm -rf LICENSE
+    rm -rf contributing.md
+    rm -rf code_of_conduct.md
+    rm -- "$0"
+    exit
+    ;;
+  *) echo "Please answer yes or no." ;;
+  esac
 done
